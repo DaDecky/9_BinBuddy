91a1fb4eb1f552edb84198c760349d42fc44f8e4
Muhammad Dicky Isra <mdikiisra98@gmail.com>
2024-08-24 20:32:48 +0700

    feat: add map with markers

---
+++ src/app/maps/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../components/Map";
+
+const Page = () => {
+  return (
+    <div className="h-screen">
+      <Map />
+    </div>
+  );
+};
+
+export default Page;
