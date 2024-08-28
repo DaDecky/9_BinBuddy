a26a5c5aac41b826795ba560223fa2245db4ac59
Muhammad Dicky Isra <mdikiisra98@gmail.com>
2024-08-28 16:48:48 +0700

    feat: navigate button

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -8,6 +8,11 @@ const Map = dynamic(() => import("../components/Map"), {
 const Page = () => {
   return (
     <div className="h-screen">
+      <div className="fixed bottom-5 left-0 right-0 text-center  z-[1000] ">
+        <button className="bg-slate-500/80 p-2 rounded-md ">
+          Navigate To Closest Bin
+        </button>
+      </div>
       <Map />
     </div>
   );
