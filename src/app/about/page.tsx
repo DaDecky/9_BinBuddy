2a9117f93110f160316cbf5265c4204dd0a4cd65
KennethRicardoC <166089884+KennethRicardoC@users.noreply.github.com>
2024-08-28 17:05:22 +0700

    Update page.tsx
    Changed 500px to 400px for Alma and Audy photo sizes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -338,7 +338,7 @@ const Page = () => {
                     </button>
 
                     {showAlma && (
-                        <img src="/Photos/Alma.jpg" alt="Alma" style={{width : '400px', height : '500px' }} className="rounded-full"></img>
+                        <img src="/Photos/Alma.jpg" alt="Alma" style={{width : '400px', height : '400px' }} className="rounded-full"></img>
                     )}
 
                     <button
@@ -348,7 +348,7 @@ const Page = () => {
                     </button>
 
                     {showAudy && (
-                        <img src="/Photos/Audy.jpg" alt="Audy" style={{width : '400px', height : '500px' }} className="rounded-full"></img>
+                        <img src="/Photos/Audy.jpg" alt="Audy" style={{width : '400px', height : '400px' }} className="rounded-full"></img>
                     )}
                 </div>
             )}
